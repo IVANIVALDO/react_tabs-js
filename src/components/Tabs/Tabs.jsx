62abd3d3952c88f1e86cae253514e1b4bf0fe509
IVANIVALDO <142198929+IVANIVALDO@users.noreply.github.com>
2025-07-18 00:42:25 -0300

    corrige acessibilidade nas tabes para ESLint

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,25 @@
-export const Tabs = () => {};
+import React from 'react';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  return (
+    <div>
+      <ul className="tabs">
+        {tabs.map(tab => (
+          <li key={tab.id}>
+            <button
+              type="button"
+              className="tab"
+              onClick={() => onTabSelected(tab)}
+            >
+              {tab.label}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <div data-cy="TabContent">{activeTab.content}</div>
+    </div>
+  );
+};
